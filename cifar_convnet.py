abf4bdb40062a7996c872b76f6a477c9587e3338
Donald Brien <dcbrien@gmail.com>
2022-06-20 13:14:14 -0400

    Test

---
+++ cifar_convnet.py
@@ -0,0 +1,3 @@
+# Testing out Jax by developing a super simple convnet to classify the cifar10 data. Hoping to learn
+# jax, haiku, and optax, but also refresh on python classes and learn some new software engineering
+# in python in general.
